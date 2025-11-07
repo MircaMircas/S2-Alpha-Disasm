eb189371ee783e70d173777f8d8f1859ea676183
MircaMircas <mikosmikus@gmail.com>
2025-11-07 12:40:00 +0100

    Update file comparison to use s2alphabuilt.bin

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -5,8 +5,9 @@ call build
 
 REM  // run fc against the original ROM
 echo -------------------------------------------------------------
-if exist s2built.bin ( fc /b s2built.bin sonic2alpha.bin
+if exist s2built.bin ( fc /b s2built.bin s2alphabuilt.bin
 ) else echo s2built.bin does not exist, probably due to an assembly error
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
 pause
+
