ff2d00a5428200d643a55c1f0df25969de5fe732
MircaMircas <mikosmikus@gmail.com>
2025-11-07 12:55:47 +0100

    Update file check and variable names in batch script

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -5,10 +5,11 @@ call build
 
 REM  // run fc against the original ROM
 echo -------------------------------------------------------------
-if exist s2built.bin ( fc /b s2built.bin sonic2alpha.bin
-) else echo s2built.bin does not exist, probably due to an assembly error
+if exist s2alphabuilt.bin ( fc /b s2alphabuilt.bin sonic2alpha.bin
+) else echo s2alphabuilt.bin does not exist, probably due to an assembly error
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
 pause
+
 
 
