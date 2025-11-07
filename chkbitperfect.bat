48b136645980fe73248d98982bcd15ade46c9842
MircaMircas <mikosmikus@gmail.com>
2025-11-07 12:46:38 +0100

    Revert file comparison target in chkbitperfect.bat

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -5,9 +5,10 @@ call build
 
 REM  // run fc against the original ROM
 echo -------------------------------------------------------------
-if exist s2built.bin ( fc /b s2built.bin s2alphabuilt.bin
+if exist s2built.bin ( fc /b s2built.bin sonic2alpha.bin
 ) else echo s2built.bin does not exist, probably due to an assembly error
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
 pause
+
 
